803614c426682c70431d1c6c0a172e7e888505fc
Thanh Le <lenguyenthanh@hotmail.com>
2024-05-11 10:50:29 +0700

    Add convenient function to PlayClient

--- modules/client/src/main/scala/PlayClient.scala
+++ modules/client/src/main/scala/PlayClient.scala
@@ -12,6 +12,7 @@ import scala.concurrent.Future
 import smithy4s.json.Json.given
 import smithy4s.schema.Schema
 import play.api.libs.ws.BodyReadable
+import scala.annotation.targetName
 
 class PlayClient(client: StandaloneWSClient, baseUrl: String)(using ExecutionContext)
     extends SearchService[Future]:
@@ -24,11 +25,19 @@ class PlayClient(client: StandaloneWSClient, baseUrl: String)(using ExecutionCon
       .post(StoreBulkTeamInput(sources))
       .map(_ => ())
 
+  @targetName("storeBulkTeamWithPair")
+  def storeBulkTeam(sources: List[(String, TeamSource)]): Future[Unit] =
+    storeBulkTeam(sources.map(TeamSourceWithId.apply.tupled))
+
   override def storeBulkStudy(sources: List[StudySourceWithId]): Future[Unit] =
     client
       .url(s"$baseUrl/store-bulk/study")
       .post(StoreBulkStudyInput(sources))
       .map(_ => ())
+
+  @targetName("storeBulkStudyWithPair")
+  def storeBulkStudy(sources: List[(String, StudySource)]): Future[Unit] =
+    storeBulkStudy(sources.map(StudySourceWithId.apply.tupled))
 
   override def storeBulkGame(sources: List[GameSourceWithId]): Future[Unit] =
     client
@@ -36,16 +45,48 @@ class PlayClient(client: StandaloneWSClient, baseUrl: String)(using ExecutionCon
       .post(StoreBulkGameInput(sources))
       .map(_ => ())
 
+  @targetName("storeBulkGameWithPair")
+  def storeBulkGame(sources: List[(String, GameSource)]): Future[Unit] =
+    storeBulkGame(sources.map(GameSourceWithId.apply.tupled))
+
   override def storeBulkForum(sources: List[ForumSourceWithId]): Future[Unit] =
     client
       .url(s"$baseUrl/store-bulk/forum")
       .post(StoreBulkForumInput(sources))
       .map(_ => ())
 
+  @targetName("storeBulkForumWithPair")
+  def storeBulkForum(sources: List[(String, ForumSource)]): Future[Unit] =
+    storeBulkForum(sources.map(ForumSourceWithId.apply.tupled))
+
   override def store(source: Source, id: String): Future[Unit] =
     client
       .url(s"$baseUrl/store/$id")
       .post(source)
+      .map(_ => ())
+
+  def storeForum(id: String, source: ForumSource): Future[Unit] =
+    client
+      .url(s"$baseUrl/store/$id")
+      .post(Source.forum(source))
+      .map(_ => ())
+
+  def storeGame(id: String, source: GameSource): Future[Unit] =
+    client
+      .url(s"$baseUrl/store/$id")
+      .post(Source.game(source))
+      .map(_ => ())
+
+  def storeStudy(id: String, source: StudySource): Future[Unit] =
+    client
+      .url(s"$baseUrl/store/$id")
+      .post(Source.study(source))
+      .map(_ => ())
+
+  def storeTeam(id: String, source: TeamSource): Future[Unit] =
+    client
+      .url(s"$baseUrl/store/$id")
+      .post(Source.team(source))
       .map(_ => ())
 
   override def refresh(index: Index): Future[Unit] =
