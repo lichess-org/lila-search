35e892475516f6aca795e923e3ae54b7a51ae065
Thanh Le <lenguyenthanh@hotmail.com>
2024-06-03 17:29:07 +0700

    Remove Query as We only need it for play module

--- modules/core/src/main/scala/model.scala
+++ modules/core/src/main/scala/model.scala
@@ -1,6 +1,8 @@
 package lila.search
 
 import com.sksamuel.elastic4s.requests.searches.SearchResponse as ESR
+import lila.search.game.{ Clocking, Sorting }
+import org.joda.time.DateTime
 
 case class Id(value: String)
 
@@ -21,3 +23,32 @@ object CountResponse:
 
   def apply(res: ESR): CountResponse =
     CountResponse(res.totalHits.toInt)
+
+case class Team(text: String)
+
+case class Game(
+    user1: Option[String] = None,
+    user2: Option[String] = None,
+    winner: Option[String] = None,
+    loser: Option[String] = None,
+    winnerColor: Option[Int] = None,
+    perf: List[Int] = List.empty,
+    source: Option[Int] = None,
+    status: Option[Int] = None,
+    turns: Range[Int] = Range.none,
+    averageRating: Range[Int] = Range.none,
+    hasAi: Option[Boolean] = None,
+    aiLevel: Range[Int] = Range.none,
+    rated: Option[Boolean] = None,
+    date: Range[DateTime] = Range.none,
+    duration: Range[Int] = Range.none,
+    clock: Clocking = Clocking(),
+    sorting: Sorting = Sorting.default,
+    analysed: Option[Boolean] = None,
+    whiteUser: Option[String] = None,
+    blackUser: Option[String] = None
+)
+
+case class Forum(text: String, troll: Boolean)
+
+case class Study(text: String, userId: Option[String])
