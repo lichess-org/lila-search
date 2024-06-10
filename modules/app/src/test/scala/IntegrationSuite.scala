e3a3c1235a88337b9573173e15cd6886d7eed868
Thanh Le <lenguyenthanh@hotmail.com>
2024-06-10 14:48:08 +0700

    Some more tests for new fields

--- modules/app/src/test/scala/IntegrationSuite.scala
+++ modules/app/src/test/scala/IntegrationSuite.scala
@@ -154,4 +154,13 @@ object IntegrationSuite extends IOSuite:
           d <- service.search(Query.game(duration = IntRange(a = 99.some, b = 101.some).some), 0, 12)
           e <- service.search(Query.game(clockInit = 100.some), 0, 12)
           f <- service.search(Query.game(clockInc = 200.some), 0, 12)
-        yield expect(a.hitIds.size == 1 && b == a && c == a && d == a && e == a && f == a)
+          g <- service.search(Query.game(clock = Clocking(initMin = 99.some, initMax = 101.some).some), 0, 12)
+          h <- service.search(Query.game(clock = Clocking(incMin = 199.some, incMax = 201.some).some), 0, 12)
+          i <- service.search(
+            Query.game(clock = Clocking(incMin = 99.some, incMax = 101.some).some, clockInc = 200.some),
+            0,
+            12
+          )
+        yield expect(
+          a.hitIds.size == 1 && b == a && c == a && d == a && e == a && f == a && g == a && h == a && i == a
+        )
