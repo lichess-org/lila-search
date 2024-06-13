85701cfea17337cbed39c71cebbe0dbfd6549a6d
Thanh Le <lenguyenthanh@hotmail.com>
2024-06-13 16:32:11 +0700

    Experiment with watch & resumeToken
    We can resume a stream of change events from a[resumeToken](https://www.mongodb.com/docs/manual/changeStreams/#std-label-change-stream-resume-token)
    So, If We save the last resume token, We're free to start/stop ingestor
    whenever We want

--- modules/ingestor/src/main/scala/ingestor.scala
+++ modules/ingestor/src/main/scala/ingestor.scala
@@ -2,8 +2,10 @@ package lila.search
 package ingestor
 
 import cats.effect.*
-import cats.syntax.all.*
 import mongo4cats.database.MongoDatabase
+import mongo4cats.operations.{ Aggregate, Filter }
+import org.bson.BsonDocument
+import mongo4cats.bson.BsonValue
 
 trait Ingestor:
   def run(): IO[Unit]
@@ -11,6 +13,27 @@ trait Ingestor:
 object Ingestor:
   def apply(db: MongoDatabase[IO]): Ingestor = new:
 
+    val eventFilter = Filter.in("operationType", List("replace", "insert"))
+    val aggregate   = Aggregate.matchBy(eventFilter)
+    val resumeToken = BsonDocument(
+      "_data",
+      BsonValue
+        .string(
+          "82666AB9B1000000012B022C0100296E5A1004AC00875A70284DF9B1268E0E8EC36E5E463C5F6964003C6D446144686A6D38000004"
+        )
+        .asJava
+    )
+
     def run(): IO[Unit] =
-      db.listCollectionNames.flatMap: names =>
-        names.toList.traverse_(name => IO.println(s"Collection: $name"))
+      db.getCollection("f_post")
+        .flatMap(
+          _.watch(aggregate)
+            .resumeAfter(resumeToken)
+            .stream
+            .evalMap: event =>
+              IO.println(
+                s"resumeToken: ${event.resumeToken} type: ${event.operationType} key: ${event.documentKey} time: ${event.clusterTime} full: ${event.fullDocument} fullbefore: ${event.fullDocumentBeforeChange}"
+              )
+            .compile
+            .drain
+        )
