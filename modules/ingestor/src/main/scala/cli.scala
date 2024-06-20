bb4d11d1c4d7ee7ea85681b76469bbb720e7003c
Thanh Le <lenguyenthanh@hotmail.com>
2024-06-20 14:53:49 +0700

    Implement IndexConfig opts

--- modules/ingestor/src/main/scala/cli.scala
+++ modules/ingestor/src/main/scala/cli.scala
@@ -3,9 +3,13 @@ package ingestor
 
 import cats.data.Validated
 import cats.effect.*
+import cats.syntax.all.*
 import com.monovore.decline.*
 import com.monovore.decline.effect.*
 
+import java.time.Instant
+
+// supported flags: dry, verbose
 object cli
     extends CommandIOApp(
       name = "lila-search-cli",
@@ -14,9 +18,24 @@ object cli
     ):
 
   override def main: Opts[IO[ExitCode]] =
-    indexOpts.map(x => IO.println(x).as(ExitCode.Success))
+    opts.indexOpt.map(x => IO.println(x).as(ExitCode.Success))
 
-  val indexOpts =
-    Opts
-      .option[String]("index", "Specify the targeted index", metavar = "string")
-      .mapValidated(x => Validated.fromEither(Index.fromString(x)).toValidatedNel)
+object opts:
+  case class IndexConfig(index: Index, since: Instant, until: Instant)
+
+  // valid since <= until
+  val indexOpt = (
+    Opts.option[Index]("index", "Index to reindex", short = "i", metavar = "index"),
+    Opts.option[Instant]("since", "Start date", short = "s", metavar = "time in epoch seconds"),
+    Opts.option[Instant]("until", "End date", short = "u", metavar = "time in epoch seconds")
+  ).mapN(IndexConfig.apply)
+
+  given Argument[Index] =
+    Argument.from("index")(x => Validated.fromEither(Index.fromString(x)).toValidatedNel)
+
+  given Argument[Instant] =
+    Argument.from("time in epoch seconds")(str =>
+      str.toLongOption.fold(Validated.invalidNel(s"Invalid epoch seconds: $str"))(x =>
+        Validated.valid(Instant.ofEpochSecond(x))
+      )
+    )
