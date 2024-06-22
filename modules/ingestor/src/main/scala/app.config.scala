80ef59c4dfdedf473e93e701afe35fcc717989d7
Thanh Le <lenguyenthanh@hotmail.com>
2024-06-22 15:16:17 +0700

    Prepare for study ingestor

--- modules/ingestor/src/main/scala/app.config.scala
+++ modules/ingestor/src/main/scala/app.config.scala
@@ -35,11 +35,12 @@ object ElasticConfig:
   private def uri = env("ELASTIC_URI").or(prop("elastic.uri")).as[String].default("http://127.0.0.1:9200")
   def config      = uri.map(ElasticConfig.apply)
 
-case class IngestorConfig(forum: IngestorConfig.Forum, team: IngestorConfig.Team)
+case class IngestorConfig(forum: IngestorConfig.Forum, team: IngestorConfig.Team, study: IngestorConfig.Study)
 
 object IngestorConfig:
   case class Forum(batchSize: Int, timeWindows: Int, startAt: Option[Long])
   case class Team(batchSize: Int, timeWindows: Int, startAt: Option[Long])
+  case class Study(batchSize: Int, timeWindows: Int, startAt: Option[Long])
 
   private object Forum:
     private def batchSize =
@@ -62,4 +63,13 @@ object IngestorConfig:
       env("INGESTOR_TEAM_START_AT").or(prop("ingestor.team.start.at")).as[Long].option
     def config = (batchSize, timeWindows, startAt).parMapN(Team.apply)
 
-  def config = (Forum.config, Team.config).parMapN(IngestorConfig.apply)
+  private object Study:
+    private def batchSize =
+      env("INGESTOR_STUDY_BATCH_SIZE").or(prop("ingestor.study.batch.size")).as[Int].default(100)
+    private def timeWindows =
+      env("INGESTOR_STUDY_TIME_WINDOWS").or(prop("ingestor.study.time.windows")).as[Int].default(10)
+    private def startAt =
+      env("INGESTOR_STUDY_START_AT").or(prop("ingestor.study.start.at")).as[Long].option
+    def config = (batchSize, timeWindows, startAt).parMapN(Study.apply)
+
+  def config = (Forum.config, Team.config, Study.config).parMapN(IngestorConfig.apply)
