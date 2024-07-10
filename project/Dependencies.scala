1b22e0fdf0242020bd8d8ac3dae2b19748429119
Scala Steward <scala_steward@virtuslab.com>
2024-07-10 17:03:02 +0000

    Update play-ahc-ws-standalone to 2.2.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -40,7 +40,7 @@ object Dependencies {
   val jsoniterCore = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.30.3"
   val jsoniterMacro = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.30.3"
 
-  val playWS = "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.2.8"
+  val playWS = "com.typesafe.play" %% "play-ahc-ws-standalone" % "2.2.9"
 
   val elastic4sJavaClient = "nl.gn0s1s" %% "elastic4s-client-esjava" % V.elastic4s
   val elastic4sCatsEffect = "nl.gn0s1s" %% "elastic4s-effect-cats"   % V.elastic4s
