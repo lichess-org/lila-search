979c0376d7c7d86daddbf8090538a1cd4f82bf93
Thanh Le <lenguyenthanh@hotmail.com>
2024-08-18 16:17:24 +0200

    Setup Game Ingestor

--- modules/ingestor/src/main/scala/ingestor.game.scala
+++ modules/ingestor/src/main/scala/ingestor.game.scala
@@ -4,6 +4,7 @@ package ingestor
 import cats.effect.*
 import cats.syntax.all.*
 import com.mongodb.client.model.changestream.FullDocument
+import com.mongodb.client.model.changestream.OperationType.*
 import io.circe.*
 import mongo4cats.circe.*
 import mongo4cats.collection.MongoCollection
@@ -14,104 +15,72 @@ import org.typelevel.log4cats.syntax.*
 
 import java.time.Instant
 import scala.concurrent.duration.*
+import mongo4cats.operations.Projection
+import mongo4cats.database.MongoDatabase
 
 trait GameIngestor:
-  // watch change events from game5 collection
-  def watch(since: Instant, until: Instant): IO[Unit]
+  // watch change events from game5 collection and ingest games into elastic search
+  def watch: fs2.Stream[IO, Unit]
 
 object GameIngestor:
 
-  def apply(games: MongoCollection[IO, DbGame])(using Logger[IO]): GameIngestor = new:
+  private val index = Index.Game
 
-    def watch(since: Instant, until: Instant): IO[Unit] =
-      changes(since, until).compile.drain
+  private val interestedOperations = List(INSERT, REPLACE).map(_.getValue) // We don't delete games
 
-    private def changes(
-        since: Instant,
-        until: Instant
-    ): fs2.Stream[IO, List[DbGame]] =
-      val batchSize  = 1000
-      val timeWindow = 10
-      games
-        .watch(aggreate(since, until))
-        .startAtOperationTime(BsonTimestamp(since.getEpochSecond.toInt, 1))
-        .batchSize(batchSize)                     // config.batchSize
+  // private val interestedFields = List(_id, F.text, F.topicId, F.troll, F.createdAt, F.userId, F.erasedAt)
+  // private val postProjection   = Projection.include(interestedFields)
+
+  private val interestedEventFields =
+    List(
+      "operationType",
+      "clusterTime",
+      "documentKey._id",
+      "fullDocument"
+    ) // TODO only include interestedFields
+  private val eventProjection = Projection.include(interestedEventFields)
+
+  private val aggregate =
+    Aggregate.project(eventProjection)
+
+  def apply(mongo: MongoDatabase[IO], elastic: ESClient[IO], store: KVStore, config: IngestorConfig.Game)(
+      using Logger[IO]
+  ): IO[GameIngestor] =
+    (mongo.getCollectionWithCodec[DbGame]("game5")).map(apply(elastic, store, config))
+
+  def apply(
+      elastic: ESClient[IO],
+      store: KVStore,
+      config: IngestorConfig.Game
+  )(games: MongoCollection[IO, DbGame])(using Logger[IO]): GameIngestor = new:
+
+    def watch: fs2.Stream[IO, Unit] =
+      fs2.Stream
+        .eval(startAt.flatTap(since => info"Starting forum ingestor from $since"))
+        .flatMap: since =>
+          changes(since)
+            .evalMap(IO.println)
+
+    private def changes(since: Option[Instant]): fs2.Stream[IO, List[DbGame]] =
+      val builder = games.watch(aggregate)
+      // skip the first event if we're starting from a specific timestamp
+      // since the event at that timestamp is already indexed
+      val skip = since.fold(0)(_ => 1)
+      since
+        .fold(builder)(x => builder.startAtOperationTime(x.asBsonTimestamp))
         .fullDocument(FullDocument.UPDATE_LOOKUP) // this is required for update event
-        .boundedStream(batchSize)
-        .groupWithin(batchSize, timeWindow.second) // config.windows
+        .batchSize(config.batchSize)              // config.batchSize
+        .boundedStream(config.batchSize)
+        .drop(skip)
+        .groupWithin(config.batchSize, config.timeWindows.second) // config.windows
         .evalTap(_.traverse_(x => info"received $x"))
-        .evalTap(_.filter(_.fullDocument.exists(_.validClock)).traverse_(x => info"count $x"))
         .map(_.toList.map(_.fullDocument).flatten)
         .evalTap(_.traverse_(x => info"clock ${x.clock}"))
         .map(_.filter(_.validClock))
         .evalTap(_.traverse_(x => info"valid clock ${x.clock}"))
 
-    private def aggreate(since: Instant, until: Instant) =
-      // games have at least 15 moves
-      val turnsFilter    = Filter.gte("fullDocument.t", 30)
-      val standardFilter = Filter.eq("fullDocument.v", 1).or(Filter.notExists("fullDocument.v"))
-      val ratedFilter    = Filter.eq("fullDocument.ra", true)
-      val noAiFilter =
-        Filter
-          .eq("fullDocument.p0.ai", 0)
-          .or(Filter.notExists("fullDocument.p0.ai"))
-          .and(Filter.eq("fullDocument.p1.ai", 0).or(Filter.notExists("fullDocument.p1.ai")))
-
-      // Filter games that finished with Mate, Resign, Stalemate, Draw, Outoftime, Timeout
-      // https://github.com/lichess-org/scalachess/blob/master/core/src/main/scala/Status.scala#L18-L23
-      val statusFilter = Filter.in("fullDocument.s", List(30, 31, 32, 33, 34, 35))
-
-      // filter games that played and ended between since and until
-      val playedTimeFilter =
-        Filter
-          .gte("fullDocument.ca", since)
-          .and(Filter.lte("fullDocument.ua", until))
-
-      val updatedStatusOnlyFilter = Filter
-        .exists("updateDescription.updatedFields.s")
-        .or(Filter.notExists("updateDescription"))
-
-      // required clock config
-      val clockFilter = Filter.exists("fullDocument.c")
-
-      // only human plays
-      val noImportGameFilter = Filter.notExists("fullDocument.pgni")
-
-      val gameFilter = standardFilter
-        .and(turnsFilter)
-        .and(ratedFilter)
-        .and(noAiFilter)
-        .and(statusFilter)
-        .and(updatedStatusOnlyFilter)
-        .and(playedTimeFilter)
-        .and(clockFilter)
-        .and(noImportGameFilter)
-
-      Aggregate.matchBy(gameFilter)
-
-      /*
-       *{
-  _id: 'TaHSAsYD',
-  us: [ 'pedro', 'ikem' ],
-  is: 'AABgAACA',
-  p0: { e: 2264, d: 3 },
-  p1: { e: 2069, d: -3 },
-  s: 35,
-  t: 97,
-  v: 1,
-  ra: true,
-  ca: ISODate('2024-02-29T17:06:28.099Z'),
-  ua: ISODate('2024-02-29T18:06:00.000Z'),
-  so: 5,
-  hp: Binary.createFromBase64('PDVvHX7zZXUV51aObVt//n+PYCv+TKzL6zXvcTbiqlrv551I5GeN/S1rL74i/K9a68ahbTaRzHvo9uTN2+dc', 0),
-  an: false,
-  c: Binary.createFromBase64('CgAA38IA6mQ=', 0),
-  cw: Binary.createFromBase64('wGAhen8MW6C28eG7gbi0S7rEzAvMPLTu1KHlhKUh18BT3zxTY9SgQYDTvHlPCf6QIyB8SJ/PQYihGoTkZtLA', 0),
-  cb: Binary.createFromBase64('vHARnEBZLL6hkblax4GSWiwXenwwssK8dk8LyS4sfqG7D1eo43kerh70+MHF8Bm0ah3leztkZ5KI4yrsrCrO8piEWFA=', 0),
-  w: true,
-  wid: 'pedro'
-}
-       * */
+    private def startAt: IO[Option[Instant]] =
+      config.startAt.fold(store.get(index.value))(Instant.ofEpochSecond(_).some.pure[IO])
 
 final case class GameSource(
     status: Int,
--- modules/ingestor/src/main/scala/ingestor.scala
+++ modules/ingestor/src/main/scala/ingestor.scala
@@ -11,7 +11,7 @@ trait Ingestor:
 
 object Ingestor:
 
-  def apply(
+  def apply1(
       lichess: MongoDatabase[IO],
       study: MongoDatabase[IO],
       local: MongoDatabase[IO],
@@ -22,13 +22,28 @@ object Ingestor:
     (
       ForumIngestor(lichess, elastic, store, config.forum),
       TeamIngestor(lichess, elastic, store, config.team),
-      StudyIngestor(study, local, elastic, store, config.study)
-    ).mapN: (forum, team, study) =>
+      StudyIngestor(study, local, elastic, store, config.study),
+      GameIngestor(lichess, elastic, store, config.game)
+    ).mapN: (forum, team, study, game) =>
       new Ingestor:
         def run() =
           fs2
-            .Stream(forum.watch, team.watch, study.watch)
+            .Stream(forum.watch, team.watch, study.watch, game.watch)
             .covary[IO]
             .parJoinUnbounded
             .compile
             .drain
+
+  def apply(
+      lichess: MongoDatabase[IO],
+      study: MongoDatabase[IO],
+      local: MongoDatabase[IO],
+      elastic: ESClient[IO],
+      store: KVStore,
+      config: IngestorConfig
+  )(using Logger[IO]): IO[Ingestor] =
+    GameIngestor(lichess, elastic, store, config.game)
+      .map: game =>
+        new Ingestor:
+          def run() =
+            game.watch.compile.drain
