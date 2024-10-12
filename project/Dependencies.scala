74c49242c409017d69d637ce23de43800a140c3c
Scala Steward <scala_steward@virtuslab.com>
2024-10-12 15:57:42 +0000

    Update mongo4cats-circe, mongo4cats-core to 0.7.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val fs2        = "3.11.0"
     val http4s     = "0.23.28"
     val iron       = "2.5.0"
-    val mongo4cats = "0.7.10"
+    val mongo4cats = "0.7.11"
   }
 
   def http4s(artifact: String)   = "org.http4s"                   %% s"http4s-$artifact"   % V.http4s
