68eb1575865fcb579b95c7c584454fa3c3da8bdd
Scala Steward <scala_steward@virtuslab.com>
2025-08-12 15:15:35 +0000

    Update ducktape to 0.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
   val log4Cats = "org.typelevel" %% "log4cats-slf4j"  % "2.7.1"
   val logback = "ch.qos.logback" % "logback-classic" % "1.5.18"
 
-  val ducktape = "io.github.arainko" %% "ducktape" % "0.2.9"
+  val ducktape = "io.github.arainko" %% "ducktape" % "0.2.10"
 
   val declineCore = "com.monovore" %% "decline" % V.decline
   val declineCatsEffect = "com.monovore" %% "decline-effect" % V.decline
