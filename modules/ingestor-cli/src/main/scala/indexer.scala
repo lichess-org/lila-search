c84a5182ba438794a98abac1b4445b67b2ea0c45
Thanh Le <lenguyenthanh@hotmail.com>
2025-11-05 22:30:15 +0100

    Clean up cli.Indexer

--- modules/ingestor-cli/src/main/scala/indexer.scala
+++ modules/ingestor-cli/src/main/scala/indexer.scala
@@ -9,14 +9,20 @@ import org.typelevel.log4cats.{ Logger, LoggerFactory }
 
 import Ingestor.given
 
-// Index execution logic
 object Indexer:
 
+  type IndexOrAll = Index | Unit
+  extension (index: IndexOrAll)
+    inline def toList: List[Index] = index match
+      case i: Index => List(i)
+      case _ => Index.values.toList
+
   def apply(opts: IndexOpts, res: AppResources, config: AppConfig)(using LoggerFactory[IO]): IO[Unit] =
+    given logger: Logger[IO] = LoggerFactory[IO].getLogger
     import opts.{ since, until, watch, dry }
     import res.*
 
-    def go(index: Index) =
+    inline def go(index: Index) =
       val ingestor = index match
         case Index.Forum =>
           ForumRepo(res.lichess, config.ingestor.forum).map:
@@ -38,35 +44,23 @@ object Indexer:
         ingestor.flatMap(_.run()) *>
         refreshIndexes(res.elastic, index).whenA(opts.refresh)
 
-    opts.index.match
-      case i: Index => go(i)
-      case _ => Index.values.toList.traverse_(go)
+    opts.index.toList.traverse_(go)
 
-  private def putMappingsIfNotExists(elastic: ESClient[IO], index: Index | Unit)(using LoggerFactory[IO]): IO[Unit] =
-    given Logger[IO] = LoggerFactory[IO].getLogger
-    def go(index: Index) =
-      Handle
-        .allow:
-          elastic
-            .indexExists(index)
-            .ifM(Logger[IO].info(s"Index ${index.value} exists, start indexing"), elastic.putMapping(index))
-        .rescue: e =>
-          Logger[IO].error(e.asException)(s"Failed to check or put mapping for ${index.value}") *>
-            e.asException.raiseError
-    index match
-      case i: Index => go(i)
-      case _ => Index.values.toList.traverse_(go)
+  private def putMappingsIfNotExists(elastic: ESClient[IO], index: Index)(using Logger[IO]): IO[Unit] =
+    Handle
+      .allow:
+        elastic
+          .indexExists(index)
+          .ifM(Logger[IO].info(s"Index ${index.value} exists, start indexing"), elastic.putMapping(index))
+      .rescue: e =>
+        Logger[IO].error(e.asException)(s"Failed to check or put mapping for ${index.value}") *>
+          e.asException.raiseError
 
-  private def refreshIndexes(elastic: ESClient[IO], index: Index | Unit)(using LoggerFactory[IO]): IO[Unit] =
-    given Logger[IO] = LoggerFactory[IO].getLogger
-    def go(index: Index) =
-      Handle
-        .allow:
-          elastic
-            .refreshIndex(index)
-        .rescue: e =>
-          Logger[IO].error(e.asException)(s"Failed to refresh index: ${index.value}") *>
-            e.asException.raiseError
-    index.match
-      case i: Index => go(i)
-      case _ => Index.values.toList.traverse_(go)
+  private def refreshIndexes(elastic: ESClient[IO], index: Index)(using Logger[IO]): IO[Unit] =
+    Handle
+      .allow:
+        elastic
+          .refreshIndex(index)
+      .rescue: e =>
+        Logger[IO].error(e.asException)(s"Failed to refresh index: ${index.value}") *>
+          e.asException.raiseError
