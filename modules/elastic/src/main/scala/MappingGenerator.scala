0ddbf91c7769bfe2d6b673f3fdf07c882e775874
Thanh Le <lenguyenthanh@hotmail.com>
2025-11-11 13:34:11 +0100

    Clean up MappingGenerator

--- modules/elastic/src/main/scala/MappingGenerator.scala
+++ modules/elastic/src/main/scala/MappingGenerator.scala
@@ -1,5 +1,6 @@
 package lila.search
 
+import cats.syntax.all.*
 import com.sksamuel.elastic4s.ElasticDsl.*
 import com.sksamuel.elastic4s.fields.*
 import smithy4s.Hints
@@ -19,48 +20,61 @@ object MappingGenerator:
         }
       case _ => Nil
 
-  private def generateTextField(fieldName: String, traitData: es.TextField): ElasticField =
-    val baseField = textField(fieldName)
-      .copy(
-        boost = traitData.boost.map(_.toDouble),
-        analyzer = traitData.analyzer
-      )
-    // If keyword subfield is specified, add it
-    traitData.keywordSubfield match
-      case Some(kw) =>
-        baseField.copy(fields = List(keywordField(kw.name).copy(normalizer = kw.normalizer)))
-      case None => baseField
+  private def generateTextField(fieldName: String, hints: Hints): Option[ElasticField] =
+    hints
+      .get[es.TextField]
+      .map: traitData =>
+        val baseField = textField(fieldName)
+          .copy(
+            boost = traitData.boost.map(_.toDouble),
+            analyzer = traitData.analyzer
+          )
+        // If keyword subfield is specified, add it
+        traitData.keywordSubfield match
+          case Some(kw) =>
+            baseField.copy(fields = List(keywordField(kw.name).copy(normalizer = kw.normalizer)))
+          case None => baseField
 
-  private def generateKeywordField(fieldName: String, traitData: es.KeywordField): ElasticField =
-    keywordField(fieldName).copy(
-      boost = traitData.boost.map(_.toDouble),
-      docValues = traitData.docValues
-    )
+  private def generateKeywordField(fieldName: String, hints: Hints): Option[ElasticField] =
+    hints
+      .get[es.KeywordField]
+      .map: traitData =>
+        keywordField(fieldName).copy(
+          boost = traitData.boost.map(_.toDouble),
+          docValues = traitData.docValues
+        )
 
-  private def generateDateField(fieldName: String, traitData: es.DateField): ElasticField =
-    dateField(fieldName).copy(
-      format = traitData.format,
-      docValues = traitData.docValues
-    )
+  private def generateDateField(fieldName: String, hints: Hints): Option[ElasticField] =
+    hints
+      .get[es.DateField]
+      .map: traitData =>
+        dateField(fieldName).copy(
+          format = traitData.format,
+          docValues = traitData.docValues
+        )
 
-  private def generateShortField(fieldName: String, traitData: es.ShortField): ElasticField =
-    shortField(fieldName).copy(docValues = traitData.docValues)
+  private def generateShortField(fieldName: String, hints: Hints): Option[ElasticField] =
+    hints
+      .get[es.ShortField]
+      .map: traitData =>
+        shortField(fieldName).copy(docValues = traitData.docValues)
 
-  private def generateIntField(fieldName: String, traitData: es.IntField): ElasticField =
-    intField(fieldName).copy(docValues = traitData.docValues)
+  private def generateIntField(fieldName: String, hints: Hints): Option[ElasticField] =
+    hints
+      .get[es.IntField]
+      .map: traitData =>
+        intField(fieldName).copy(docValues = traitData.docValues)
 
-  private def generateBooleanField(fieldName: String, traitData: es.BooleanField): ElasticField =
-    booleanField(fieldName).copy(docValues = traitData.docValues)
+  private def generateBooleanField(fieldName: String, hints: Hints): Option[ElasticField] =
+    hints
+      .get[es.BooleanField]
+      .map: traitData =>
+        booleanField(fieldName).copy(docValues = traitData.docValues)
 
   private def generateField(fieldName: String, hints: Hints): Option[ElasticField] =
-    import lila.search.es.*
-
-    // Check for ES field type traits using implicit ShapeTag
-    hints
-      .get(using TextField)
-      .map(generateTextField(fieldName, _))
-      .orElse(hints.get(using KeywordField).map(generateKeywordField(fieldName, _)))
-      .orElse(hints.get(using DateField).map(generateDateField(fieldName, _)))
-      .orElse(hints.get(using ShortField).map(generateShortField(fieldName, _)))
-      .orElse(hints.get(using IntField).map(generateIntField(fieldName, _)))
-      .orElse(hints.get(using BooleanField).map(generateBooleanField(fieldName, _)))
+    generateTextField(fieldName, hints) <+>
+      generateKeywordField(fieldName, hints) <+>
+      generateDateField(fieldName, hints) <+>
+      generateShortField(fieldName, hints) <+>
+      generateIntField(fieldName, hints) <+>
+      generateBooleanField(fieldName, hints)
