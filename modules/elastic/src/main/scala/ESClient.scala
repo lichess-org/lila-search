13970e39c8a8e6af10ed6c296886a811945347d5
Thanh Le <lenguyenthanh@hotmail.com>
2025-11-11 21:04:27 +0100

    Keep study2 source

--- modules/elastic/src/main/scala/ESClient.scala
+++ modules/elastic/src/main/scala/ESClient.scala
@@ -88,10 +88,10 @@ object ESClient:
         .whenA(ids.nonEmpty)
 
     def putMapping(index: Index): RaiseF[Unit] =
-      client
+      dropIndex(index) *> client
         .execute:
           createIndex(index.value)
-            .mapping(properties(index.mapping)) // all false
+            .mapping(properties(index.mapping).source(index.keepSource))
             .shards(5)
             .replicas(0)
             .refreshInterval(index.refreshInterval)
@@ -108,5 +108,5 @@ object ESClient:
         .flatMap(_.toResult)
         .map(_.exists)
 
-    // private def dropIndex(index: Index) =
-    //   client.execute(deleteIndex(index.value))
+    private def dropIndex(index: Index) =
+      client.execute(deleteIndex(index.value))
