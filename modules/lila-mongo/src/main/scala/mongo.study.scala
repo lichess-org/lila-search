9cc92b90a37634fe3a880e19513057590e339cfb
Thanh Le <lenguyenthanh@hotmail.com>
2025-11-15 09:56:21 +0100

    Move DbStudy and friends to study2

--- modules/lila-mongo/src/main/scala/mongo.study.scala
+++ modules/lila-mongo/src/main/scala/mongo.study.scala
@@ -3,9 +3,6 @@ package ingestor
 
 import cats.effect.IO
 import cats.syntax.all.*
-import io.circe.*
-import mongo4cats.bson.Document
-import mongo4cats.circe.*
 import mongo4cats.database.MongoDatabase
 import org.typelevel.log4cats.syntax.*
 import org.typelevel.log4cats.{ Logger, LoggerFactory }
@@ -63,70 +60,3 @@ object StudyRepo:
             def reason =
               if chapterMap.contains(study.id) then "" else "missing chapter data; "
             info"failed to prepare study data for ${study.id}: $reason".whenA(data.isEmpty)
-
-  object F:
-    val name = "name"
-    val likes = "likes"
-    val members = "members"
-    val ownerId = "ownerId"
-    val visibility = "visibility"
-    val topics = "topics"
-    val createdAt = "createdAt"
-    val updatedAt = "updatedAt"
-    val rank = "rank"
-    val from = "rank"
-    val oplogDeleteId = "o._id"
-    val oplogUpdateId = "o2._id"
-    val oplogLikes = "o.diff.u.likes"
-    val oplogRank = "o.diff.u.rank"
-
-case class StudyLikesOnly(id: Id, likes: Int, rank: Option[Instant]):
-  inline def toMap: Map[String, Any] =
-    Map("likes" -> likes) ++ rank.map(r => "rank" -> SearchDateTime.fromInstant(r))
-
-object StudyLikesOnly:
-  given HasDocId[StudyLikesOnly] with
-    extension (a: StudyLikesOnly) def docId: Option[String] = Some(a.id.value)
-
-  def fromDoc(doc: Document): Option[StudyLikesOnly] =
-    import StudyRepo.F
-    val id = doc.getNestedAs[String](F.oplogUpdateId).map(Id.apply)
-    val rank = doc.getNestedAs[Instant](F.oplogRank)
-    val likes = doc.getNestedAs[Int](F.oplogLikes)
-    (id, likes).mapN(StudyLikesOnly(_, _, rank))
-
-case class DbStudy(
-    id: String, // _id
-    name: String,
-    ownerId: String,
-    members: Option[Map[String, Json]], // Map where we only care about keys
-    visibility: Option[String],
-    topics: Option[List[String]],
-    likes: Option[Int],
-    rank: Option[Instant],
-    createdAt: Option[Instant],
-    updatedAt: Option[Instant],
-    from: Option[String] = None
-):
-  def memberIds: List[String] = members.fold(Nil)(_.keys.toList)
-
-object DbStudy:
-  import StudyRepo.F
-  given Decoder[DbStudy] =
-    Decoder.forProduct11(
-      _id,
-      F.name,
-      F.ownerId,
-      F.members,
-      F.visibility,
-      F.topics,
-      F.likes,
-      F.rank,
-      F.createdAt,
-      F.updatedAt,
-      F.from
-    )(DbStudy.apply)
-
-  // We don't write to the database so we don't need to implement this
-  given Encoder[DbStudy] = new:
-    def apply(a: DbStudy): Json = ???
--- modules/lila-mongo/src/main/scala/mongo.study2.scala
+++ modules/lila-mongo/src/main/scala/mongo.study2.scala
@@ -3,6 +3,7 @@ package ingestor
 
 import cats.effect.IO
 import cats.syntax.all.*
+import io.circe.*
 import mongo4cats.bson.Document
 import mongo4cats.circe.*
 import mongo4cats.collection.MongoCollection
@@ -12,12 +13,11 @@ import org.typelevel.log4cats.syntax.*
 import org.typelevel.log4cats.{ Logger, LoggerFactory }
 
 import java.time.Instant
-
-import Repo.*
 import scala.annotation.nowarn
 
+import Repo.*
+
 object Study2Repo:
-  import StudyRepo.F
 
   private val interestedfields =
     List(
@@ -30,7 +30,8 @@ object Study2Repo:
       F.likes,
       F.rank,
       F.createdAt,
-      F.updatedAt
+      F.updatedAt,
+      F.from
     )
 
   private val indexDocProjection = Projection.include(interestedfields)
@@ -120,3 +121,72 @@ object Study2Repo:
             fs2.Stream.unfold(now)(s => (s, s.plusSeconds(config.interval.toSeconds)).some)
           )).zipWithNext
         .map((since, until) => since -> until.get)
+
+  object F:
+    val name = "name"
+    val likes = "likes"
+    val members = "members"
+    val ownerId = "ownerId"
+    val visibility = "visibility"
+    val topics = "topics"
+    val createdAt = "createdAt"
+    val updatedAt = "updatedAt"
+    val rank = "rank"
+    val from = "from"
+    val oplogDeleteId = "o._id"
+    val oplogUpdateId = "o2._id"
+    val oplogLikes = "o.diff.u.likes"
+    val oplogRank = "o.diff.u.rank"
+
+case class StudyLikesOnly(id: Id, likes: Int, rank: Option[Instant]):
+  inline def toMap: Map[String, Any] =
+    Map("likes" -> likes) ++ rank.map(r => "rank" -> SearchDateTime.fromInstant(r))
+
+object StudyLikesOnly:
+  import Study2Repo.F
+  given HasDocId[StudyLikesOnly] with
+    extension (a: StudyLikesOnly) def docId: Option[String] = Some(a.id.value)
+
+  def fromDoc(doc: Document): Option[StudyLikesOnly] =
+    val id = doc.getNestedAs[String](F.oplogUpdateId).map(Id.apply)
+    val rank = doc.getNestedAs[Instant](F.oplogRank)
+    val likes = doc.getNestedAs[Int](F.oplogLikes)
+    (id, likes).mapN(StudyLikesOnly(_, _, rank))
+
+case class DbStudy(
+    id: String, // _id
+    name: String,
+    ownerId: String,
+    members: Option[Map[String, Json]], // Map where we only care about keys
+    visibility: Option[String],
+    topics: Option[List[String]],
+    likes: Option[Int],
+    rank: Option[Instant],
+    createdAt: Option[Instant],
+    updatedAt: Option[Instant],
+    from: Option[String] = None
+):
+  def memberIds: List[String] = members.fold(Nil)(_.keys.toList)
+  // https://github.com/lichess-org/lila/blob/71f127800da448ef79f7a5f868c16608110c9c7c/modules/study/src/main/BSONHandlers.scala#L377
+  def isBroadcast: Boolean = from.exists(_.startsWith("relay"))
+
+object DbStudy:
+  import Study2Repo.F
+  given Decoder[DbStudy] =
+    Decoder.forProduct11(
+      _id,
+      F.name,
+      F.ownerId,
+      F.members,
+      F.visibility,
+      F.topics,
+      F.likes,
+      F.rank,
+      F.createdAt,
+      F.updatedAt,
+      F.from
+    )(DbStudy.apply)
+
+  // We don't write to the database so we don't need to implement this
+  given Encoder[DbStudy] = new:
+    def apply(a: DbStudy): Json = ???
