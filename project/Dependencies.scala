154836452d0e6ed2c229493e658dc21d6f1a37c7
Scala Steward <scala_steward@virtuslab.com>
2026-07-08 17:33:09 +0000

    Update http4s-client, http4s-ember-client, ... to 0.23.36

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies:
     val elastic4s = "9.3.0"
     val fs2 = "3.13.0"
     val fs2Data = "1.14.0"
-    val http4s = "0.23.34"
+    val http4s = "0.23.36"
     val mongo4cats = "0.7.18"
     val otel4sCore = "1.0.0"
     val otel4sSdk = "0.19.0"
